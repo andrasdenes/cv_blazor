6bcb7c15e8c89a9aa1d9829f6952bb8922b95c0f
Andras Ferenc Denes <andras.denes@evosoft.com>
2023-03-07 08:21:23 +0100

    no cache on httpclient

--- CV/CV/Client/Program.cs
+++ CV/CV/Client/Program.cs
@@ -8,7 +8,13 @@ namespace CV
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddHttpClient<ICvClient, CvClient>(client=>client.BaseAddress=new Uri("https://localhost:7064/"));
+            builder.Services.AddHttpClient<ICvClient, CvClient>(
+                client=>
+                {
+                    client.BaseAddress = new Uri("https://localhost:7064/");
+                    client.DefaultRequestHeaders.Add("Cache-Control", "no-cache");
+                }
+                );
 
             builder.Services.AddTransient<IJobService, JobService>();
 
